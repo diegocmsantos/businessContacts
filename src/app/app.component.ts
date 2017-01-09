9ef667d53edeb0d0d63899841c585ff4e13c56b7
Diego Maia <diegocmsantos@gmail.com>
2017-01-09 11:41:13 -0300

    Setup firebase and list companies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+
+import { Business } from './models/Business';
+import { Category } from './models/Category';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ FirebaseService ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  businesses: Business[];
+  categories: Category[];
+
+  constructor(private _firebaseService: FirebaseService) {}
+
+  ngOnInit() {
+  	this._firebaseService.getBusinesses()
+  		.subscribe(businesses => this.businesses = businesses);
+
+  	this._firebaseService.getCategories()
+  		.subscribe(categories => this.categories = categories);
+  }
 }
